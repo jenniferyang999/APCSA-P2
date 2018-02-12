92d61830e294f8778d896715a0588b1433c44ab9
jenniferyang999 <jnnfryang@gmail.com>
2018-02-11 16:16:57 -0800

    2.11.18 4:16pm

--- Unit4/Unit4-Assignments/Name.java
+++ Unit4/Unit4-Assignments/Name.java
@@ -9,37 +9,38 @@ import static java.lang.System.*;
 public class Name
 {
 	private String name;
+	private String first, last;
 
 	public Name()
 	{
-
-
+		
 	}
 
 	public Name(String s)
 	{
-
-
+		name = s;
 	}
 
    public void setName(String s)
    {
-
-
+	   name = s;
+	   int thing = s.indexOf(" ");  //for Sally Baker, thing = 5
+	   first = s.substring(0, thing);
+	   last = s.substring(thing + 1, s.length());
    }
 
 	public String getFirst()
 	{
-		return "";
+		return first;
 	}
 
 	public String getLast()
 	{
-		return "";
+		return last;
 	}
 
  	public String toString()
  	{
- 		return "";
+ 		return name;
 	}
 }
--- Unit4/Unit4-Assignments/StringChecker.java
+++ Unit4/Unit4-Assignments/StringChecker.java
@@ -9,42 +9,44 @@ import static java.lang.System.*;
 public class StringChecker
 {
 	private String word;
+	private int check1, check2;
 
 	public StringChecker()
 	{
-
 
 	}
 
 	public StringChecker(String s)
 	{
-
-
+		word = s;
 	}
 
    public void setString(String s)
    {
-   	word=s;
+	    word=s;
    }
 
 	public boolean findLetter(char c)
 	{
-
-
-
+		check1 = word.indexOf(c);
+		if (check1 != -1) {
+			return true;
+		}
 		return false;
 	}
 
 	public boolean findSubString(String s)
 	{
-
-
-
+		check2 = word.indexOf(s);
+		if (check2 != -1) {
+			return true;
+		}
 		return false;
 	}
 
  	public String toString()
  	{
- 		return "\n\n";
+ 		out.println(word);
+ 		return "\n";
 	}
 }
